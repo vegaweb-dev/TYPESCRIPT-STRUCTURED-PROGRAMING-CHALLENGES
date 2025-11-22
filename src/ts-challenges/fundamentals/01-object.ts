5beb92d33f04348198c8cc35a13e4faac914da4e
cvega <carlosjuniorrvega@hotmail.com>
2025-11-22 17:50:25 -0500

    feat(fundamentals): solve challenge 01-object. CV

---
+++ src/ts-challenges/fundamentals/01-object.ts
@@ -0,0 +1,13 @@
+
+interface Movie {
+    name:string,
+    duration:number,
+    isReleased:boolean
+}
+const movie:Movie = {
+    name:"this is a name",
+    duration:34,
+    isReleased:true
+}
+
+module.exports={movie}
