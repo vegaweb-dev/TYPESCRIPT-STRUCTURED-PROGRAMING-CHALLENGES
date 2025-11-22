549b789cdf55b08002e39ec586ee4234ffc3fe1b
cvega <carlosjuniorrvega@hotmail.com>
2025-11-22 15:45:05 -0500

    chore(): set project up and test. CV

---
+++ index.ts
@@ -0,0 +1,5 @@
+const data = require("./src/ts-challenges/fundamentals/fundamentals")
+
+console.log(data.str)
+console.log(data.price)
+console.log(data.trueOrFalse)
---
+++ src/ts-challenges/fundamentals/fundamentals.ts
@@ -0,0 +1,19 @@
+
+/*
+Declara tres variables en TypeScript:
+producto → tipo string
+precio → tipo number
+enStock → tipo boolean
+Luego imprime sus valores usando console.log.*/
+
+
+const  str: string="this is a string"
+const  price: number = 23
+const trueOrFalse: boolean = true
+
+module.exports={
+    str,
+    price,
+    trueOrFalse
+}
+
