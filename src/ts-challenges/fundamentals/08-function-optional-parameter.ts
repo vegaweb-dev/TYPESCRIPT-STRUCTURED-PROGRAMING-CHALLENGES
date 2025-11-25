60e242348b27ccb985a911736745e17623fca968
cvega <carlosjuniorrvega@hotmail.com>
2025-11-25 17:45:52 -0500

    feat(fundamentals): solve challenge 08-function-optional-parameter. CV

---
+++ src/ts-challenges/fundamentals/08-function-optional-parameter.ts
@@ -0,0 +1,14 @@
+/*
+Create a TypeScript function named formatUser that receives:
+a required parameter username: string
+an optional parameter role?: string
+The function must return:
+"username (role)" if role is provided
+"username" if no role is provided
+*/
+
+
+
+const formatUser =(username:string,role?:string):string=>role? `${username} (${role})` : `${username}`
+
+module.exports={formatUser,}
