f356433471f2f207e10c49960de9f8707b90e05e
cvega <carlosjuniorrvega@hotmail.com>
2025-11-25 15:32:31 -0500

    feat(fundamentals): solva challenge 06-function-optional-parameters. CV

---
+++ src/ts-challenges/fundamentals/06-function-optional-parameters.ts
@@ -0,0 +1,15 @@
+/*
+Write a TypeScript function formatTitle that:
+Receives a required title: string
+Receives an optional category: string
+Returns:
+"TITLE - CATEGORY" when category exists
+"TITLE" when category is not provided
+Expected behavior example (DO NOT CODE THIS, JUST FOR CLARITY):
+formatTitle("Introduction") → "Introduction"
+formatTitle("Testing", "QA") → "Testing - QA"
+*/
+
+const formatTitle=(title:string, category?:string):string=>category? `${title}-${category}`: `${title}`
+
+module.exports={formatTitle,}
