f112ae0cb698b781c19600b93f2085dc9514517a
cvega <carlosjuniorrvega@hotmail.com>
2025-11-25 15:10:46 -0500

    feat(fundamentals): solve challenge 05-function-optional-parameters. CV

---
+++ src/ts-challenges/fundamentals/05-function-optional-parameter.ts
@@ -0,0 +1,20 @@
+/*
+Create a function named formatMessage with:
+Parameter msg: string (required)
+Parameter prefix?: string (optional)
+Return type: string
+"prefix: msg" → if prefix exists
+"msg" → if prefix is not provided
+Finally, print:
+formatMessage("System ready")
+formatMessage("Connection lost", "ERROR")
+*/
+
+
+const formatMessage =(msg:string, prefix?:string):string=>{
+    return prefix? `${prefix}:${msg}`:`${msg}`
+}
+console.log(formatMessage("System ready"))
+console.log(formatMessage("Connection lost","Error"))
+
+module.exports={formatMessage,}
