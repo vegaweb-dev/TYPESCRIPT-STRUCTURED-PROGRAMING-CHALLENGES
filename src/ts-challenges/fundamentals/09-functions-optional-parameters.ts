69322d44d7e03ad519c27c6fde026d1e09218424
cvega <carlosjuniorrvega@hotmail.com>
2025-11-25 17:46:35 -0500

    feat(fundamentals): solve challenge 09-functions-optional-parameter. CV

---
+++ src/ts-challenges/fundamentals/09-functions-optional-parameters.ts
@@ -0,0 +1,11 @@
+/*
+Implement a function named formatEmail that receives username: string and an optional domain?: string.
+If domain is provided, return ${username}@${domain}.
+If domain is not provided, return ${username}@example.com.
+Ensure proper TypeScript types and that the function returns a string.
+*/
+
+
+const formatEmail =(username:string, domain?:string):string=>domain ? `${username}@${domain}`:`${username}`
+
+module.exports={formatEmail,}
