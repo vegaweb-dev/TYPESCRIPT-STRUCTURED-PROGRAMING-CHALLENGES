1f8f46f85261502569ff90890fd9828e47e1b107
cvega <carlosjuniorrvega@hotmail.com>
2025-11-25 17:47:23 -0500

    feat(fundamentals): solve challenge 10-functions-optional-parameter. CV

---
+++ src/ts-challenges/fundamentals/10-functions-optional-parameters.ts
@@ -0,0 +1,13 @@
+/*
+Create a TypeScript function called formatProduct that receives:
+productName: string (required)
+price?: number (optional)
+The function must return:
+"productName - $price" if price is provided
+"productName" if price is not provided
+*/
+
+
+const formatProduct =(productName:string, price?:number)=> price ? `${productName} - $${price}`:`${productName}`
+
+module.exports = {formatProduct,}
