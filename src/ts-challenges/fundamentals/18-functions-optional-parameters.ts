c8054b098d6d008a9b07e1cf957d63b073d62d2d
cvega <carlosjuniorrvega@hotmail.com>
2025-11-26 13:15:09 -0500

    feat(fundamentals): solve challenge 18-functions-optional-parameters. CV

---
+++ src/ts-challenges/fundamentals/18-functions-optional-parameters.ts
@@ -0,0 +1,15 @@
+/*
+Create a typed TypeScript function that receives:
+name: string
+age?: number (optional)
+The function must return a string with one of the following formats:
+If age is provided → "Juan is 30 years old"
+If age is NOT provided → "Juan did not provide an age"
+*/
+
+
+const formatSentence =(name:string, age?:number):string=>age? `${name} is ${age} years old` : `${name} did not provide an age`
+
+
+
+module.exports = {formatSentence,}
