4215172dad74829e88065c48df411e1c1325fbe3
cvega <carlosjuniorrvega@hotmail.com>
2025-11-27 10:38:58 -0500

    (style): set prettier as default formatter with new rules and format 21-funtions-default-parameters with new format rules. CV

---
+++ src/ts-challenges/fundamentals/21-functions-default-parameters.ts
@@ -0,0 +1,14 @@
+/*
+Create a typed TypeScript function named formatAddress that receives:
+street: string
+city?: string (optional, default "Unknown City")
+country?: string (optional, default "Unknown Country")
+The function must return a single string in the exact format:
+"street, city, country"
+*/
+
+const formatAddress = (
+	street: string,
+	city: string = "Unknown City",
+	country: string = "Unknown Country"
+): string => `${street}, ${city}, ${country}`;
