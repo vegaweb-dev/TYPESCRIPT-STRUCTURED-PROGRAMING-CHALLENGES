7bd1d265f4522c55c57ccd9ba1cfab782c7036e0
cvega <carlosjuniorrvega@hotmail.com>
2025-11-27 09:57:53 -0500

    feat(fundamentals): solve challenge 20-functions-default-parameters. CV

---
+++ src/ts-challenges/fundamentals/20-functions-default-parameters.ts
@@ -0,0 +1,12 @@
+/*
+Create a typed TypeScript function named calculateDiscount that receives:
+price: number
+discount?: number (optional, default value 0.1 → 10%)
+The function should return the final price after applying the discount.
+*/
+
+
+const calculateDiscount =(price:number, discount:number=0.10): number=>price -(price*discount);
+
+
+module.exports={calculateDiscount,}
