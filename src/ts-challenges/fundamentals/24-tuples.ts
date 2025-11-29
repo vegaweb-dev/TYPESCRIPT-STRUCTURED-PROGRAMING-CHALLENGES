2998f7c6608814139fcfcb1a408106d0708be957
cvega <carlosjuniorrvega@hotmail.com>
2025-11-29 12:10:17 -0500

    feat(fundamentals): solve challenge 24-tuples. CV

---
+++ src/ts-challenges/fundamentals/24-tuples.ts
@@ -0,0 +1,17 @@
+/*
+Create a typed TypeScript function named getUserTuple that receives:
+name: string
+age: number
+The function must:
+Return a tuple of exactly two elements:
+[name, age]
+Example:
+getUserTuple("Ana", 30) → ["Ana", 30]
+*/
+
+type typeOfOutput = [string,number]
+const getUserTuple = (name:string, age:number):typeOfOutput =>{
+    return [name, age] as typeOfOutput
+}
+
+module.exports = {getUserTuple,}
