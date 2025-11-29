3c047ac7cc44f766b49430442fb0c6e5fe2de7d1
cvega <carlosjuniorrvega@hotmail.com>
2025-11-29 13:22:45 -0500

    feat(fundamentals): solve challenge 26-arrays-tuples. CV

---
+++ src/ts-challenges/fundamentals/26-arrays-tuples.ts
@@ -0,0 +1,14 @@
+/*
+Create a typed TypeScript function named sumNumbers that receives:
+values: number[]
+The function must:
+Return the sum of all numbers in the array.
+If the array is empty, return 0.
+Examples:
+sumNumbers([1, 2, 3]) → 6
+sumNumbers([]) → 0
+*/
+
+const sumNumbers = (values:number[]):number=>values.length===0 ? 0 : values.reduce((previousValue,currentValue)=>previousValue+currentValue);
+
+module.exports={sumNumbers,}
