ec9004c943ef212ebfa7095b4af0aa34a9178ece
cvega <carlosjuniorrvega@hotmail.com>
2025-11-29 12:23:19 -0500

    feat(fundamentals): solve challenge 25-arrays-tuples. CV

---
+++ src/ts-challenges/fundamentals/25-arrays-tuples.ts
@@ -0,0 +1,15 @@
+/*
+Create a typed TypeScript function named filterLongWords that receives:
+words: string[]
+minLength: number
+The function must:
+Return a new array containing only the words whose length is greater than or equal to minLength.
+Examples:
+filterLongWords(["hi","hello","typescript"], 5) → ["hello","typescript"]
+filterLongWords(["a","bb","ccc"], 2) → ["bb","ccc"]
+*/
+
+
+const filterLongWords =(words:string[], minLength: number):string[]=>words.filter(el=>el.length>=minLength)
+
+module.exports = {filterLongWords}
